76ee1be1547978d50d96319d0b104344db215437
liambilbo <guillermoalvarez93@gmail.com>
2019-11-01 07:06:23 +0100

    Init

---
+++ spark-streaming-consumer/src/main/java/com.dxc.bankia.event/functions/ApplyEnrichment.java
@@ -0,0 +1,100 @@
+package com.dxc.bankia.event.functions;
+
+import com.dxc.bankia.event.objects.EventExecuted;
+import com.dxc.bankia.model.Event;
+import com.dxc.bankia.services.FinderService;
+import com.dxc.bankia.services.FinderServiceImpl;
+import com.dxc.bankia.services.OutputChannelImpl;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.sql.Row;
+import org.kie.api.KieServices;
+import org.kie.api.builder.ReleaseId;
+import org.kie.api.command.Command;
+import org.kie.api.runtime.*;
+
+import java.util.*;
+
+/**
+ *
+ * Create Kie container , base and session
+ * Apply enrichment rules
+ *
+ * @see KieSession
+ * @see
+ */
+public class ApplyEnrichment extends BaseKieContainer implements FlatMapFunction<Iterator<Event>, EventExecuted> {
+
+
+    private String groupId = "com.dxc.bankia";
+    private String  artifactId = "traffic-enrichment-rules-kjar";
+    private String  version = "1.2.0";
+    private FinderService finderService=new FinderServiceImpl();
+    private OutputChannelImpl errorServiceChannel = new OutputChannelImpl();
+    private OutputChannelImpl filterServiceChannel = new OutputChannelImpl();
+
+
+   public ApplyEnrichment(String groupId, String artifactId, String version){
+        this.groupId=groupId;
+        this.artifactId=artifactId;
+        this.version=version;
+    }
+    @Override
+    public Iterator<EventExecuted> call(Iterator<Event> rowIte) throws Exception {
+       System.out.println("Start kie session");
+
+        System.setProperty("kie.maven.settings.custom", "/root/.m2/settings.xml");
+        //Initlized kie base
+       // Logger LOGGER = LogManager.getLogger(ApplyEnrichment.class);
+        KieServices ks = KieServices.Factory.get();
+        ReleaseId releaseId = ks.newReleaseId(groupId, artifactId, version);
+        StatelessKieSession statelessKieSession = this.createStatelessSession("enrichmentStatelessSession",releaseId);
+        //FinderService finderService=new FinderServiceImpl
+        //Get Blog type from kie base
+        statelessKieSession.setGlobal("finderService", finderService);
+        statelessKieSession.registerChannel("error-channel",errorServiceChannel);
+        statelessKieSession.registerChannel("filter-channel",filterServiceChannel);
+
+        List output = new ArrayList();
+
+        System.out.println("Begining event iteration");
+
+        while (rowIte.hasNext()) {
+            Event event= rowIte.next();
+            System.out.println("Adding event");
+
+
+            Command newInsertOrder = ks.getCommands().newInsert(event, "eventOut");
+            Command newFireAllRules = ks.getCommands().newFireAllRules("outFired");
+            List<Command> cmds = new ArrayList<Command>();
+            cmds.add(newInsertOrder);
+            cmds.add(newFireAllRules);
+            ExecutionResults execResults = statelessKieSession.execute(ks.getCommands().newBatchExecution(cmds));
+
+            event = (Event)execResults.getValue("eventOut");
+            int fired = (Integer)execResults.getValue("outFired");
+
+            EventExecuted executed=new EventExecuted(event,fired);
+            if (errorServiceChannel.hasMessages()) {
+                errorServiceChannel.getMessages().stream().forEach((e) -> executed.addError(e));
+                errorServiceChannel.getMessages().clear();
+                executed.setWithErrors(true);
+            }
+
+            if (filterServiceChannel.hasMessages()) {
+                filterServiceChannel.getMessages().stream().forEach((e) -> executed.addFiltered(e));
+                filterServiceChannel.getMessages().clear();
+                executed.setFiltered(true);
+            }
+
+            if (!executed.isWithErrors() && !executed.isFiltered()) {
+                executed.setEnriched(true);
+            }
+
+            output.add(executed);
+
+        };
+        System.out.println("Finishing  map partitions");
+        return output.iterator();
+    }
+}
+
---
+++ spark-streaming-consumer/src/main/java/com.dxc.bankia.event/functions/BaseKieContainer.java
@@ -0,0 +1,117 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dxc.bankia.event.functions;
+
+import org.kie.api.KieBase;
+import org.kie.api.KieServices;
+import org.kie.api.builder.KieScanner;
+import org.kie.api.builder.Message;
+import org.kie.api.builder.ReleaseId;
+import org.kie.api.builder.Results;
+import org.kie.api.runtime.ClassObjectFilter;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.StatelessKieSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Collection;
+import java.util.List;
+
+import java.util.Scanner;
+
+/**
+ * Base class to Kie Elements creation
+ *
+ */
+public class BaseKieContainer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(BaseKieContainer.class);
+    
+    /**
+     * For performance purposes, we keep a cached container. Please note that 
+     * this is not Thread Safe at all!.
+     */
+    private static KieContainer cachedKieContainer;
+
+    protected KieSession createDefaultSession() {
+        return this.createContainer(null).newKieSession();
+    }
+    
+    protected KieBase createKnowledgeBase(String name,ReleaseId releaseId) {
+        KieContainer kContainer = this.createContainer(releaseId);
+        KieBase kbase = kContainer.getKieBase(name);
+        
+        if (kbase == null){
+            throw new IllegalArgumentException("Unknown Kie Base with name '"+name+"'");
+        }
+        
+        return kbase;
+    }
+
+    protected KieSession createSession(String name,ReleaseId releaseId) {
+        
+        KieContainer kContainer = this.createContainer(releaseId);
+        KieSession ksession = kContainer.newKieSession(name);
+        
+        if (ksession == null){
+            throw new IllegalArgumentException("Unknown Session with name '"+name+"'");
+        }
+        
+        return ksession;
+    }
+
+    protected StatelessKieSession createStatelessSession(String name,ReleaseId releaseId) {
+
+        KieContainer kContainer = this.createContainer(releaseId);
+        StatelessKieSession ksession = kContainer.newStatelessKieSession(name);
+
+        if (ksession == null){
+            throw new IllegalArgumentException("Unknown Session with name '"+name+"'");
+        }
+
+        return ksession;
+    }
+
+    protected <T> Collection<T> getFactsFromKieSession(KieSession ksession, Class<T> classType) {
+        return (Collection<T>) ksession.getObjects(new ClassObjectFilter(classType));
+    }
+
+    
+    private KieContainer createContainer(ReleaseId releaseId ){
+
+
+        if (cachedKieContainer != null){
+            return cachedKieContainer;
+        }
+
+        System.setProperty("kie.maven.settings.custom", "/root/.m2/settings.xml");
+
+        KieServices ks = KieServices.Factory.get();
+        KieContainer kContainer = ks.newKieContainer(releaseId);
+        //The KieContainer is wrapped by a KieScanner.
+        //Note that we are never starting the KieScanner because we want to control
+        //when the upgrade process kicks in.
+        KieScanner scanner = ks.newKieScanner(kContainer);
+        scanner.start(30_000L);
+        // kieScanner.scanNow();
+
+        Results results = kContainer.verify();
+        
+        if (results.hasMessages(Message.Level.WARNING, Message.Level.ERROR)){
+            List<Message> messages = results.getMessages(Message.Level.WARNING, Message.Level.ERROR);
+            for (Message message : messages) {
+                System.out.printf("[%s] - %s[%s,%s]: %s", message.getLevel(), message.getPath(), message.getLine(), message.getColumn(), message.getText());
+            }
+            
+            throw new IllegalStateException("Compilation errors were found. Check the logs.");
+        }
+        
+        cachedKieContainer = kContainer;
+        return cachedKieContainer;
+    }
+
+}
